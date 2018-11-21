32365589d358cfc603ed68290ca588685b557f79
Yuuki <806393858@qq.com>
2018-11-21 21:39:19 +0800

    ribbon

--- cloud-custom/src/main/java/com/yuuki/cloud/cloudcustom/api/RestTemplateApiController.java
+++ cloud-custom/src/main/java/com/yuuki/cloud/cloudcustom/api/RestTemplateApiController.java
@@ -1,4 +1,34 @@
 package com.yuuki.cloud.cloudcustom.api;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class RestTemplateApiController {
+
+
+    @Autowired
+    RestTemplate restTemplatel;
+
+
+    @GetMapping("/msg")
+    public String getMsg(){
+        return restTemplatel.getForEntity("http://provide/msg",String.class).getBody();
+    }
+
+    @PostMapping("/param")
+    public String getMsgAndParam(String param){
+        MultiValueMap<String,Object> params = new LinkedMultiValueMap<>();
+        params.add("param",param);
+        return restTemplatel.postForEntity("http://provide/param",params,String.class).getBody();
+    }
+
+
 }
--- cloud-provide/src/main/java/com/yuuki/cloud/cloudprovide/controller/MsgController.java
+++ cloud-provide/src/main/java/com/yuuki/cloud/cloudprovide/controller/MsgController.java
@@ -1,4 +1,23 @@
 package com.yuuki.cloud.cloudprovide.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MsgController {
+
+
+    @GetMapping("/msg")
+    public String getMsg(){
+        return "this is provide msg";
+    }
+
+    @PostMapping("/param")
+    public String getMsgAndParam(String param){
+        return "this is provide msg,param is " + param;
+    }
+
+
+
 }
